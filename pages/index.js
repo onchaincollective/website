82c32d1d0e61514d160585152bb60afc12b3a9fd
Nitin R Gupta <nitinramachandragupta@gmail.com>
2021-09-24 20:53:19 +0530

    meta tags

--- pages/index.js
+++ pages/index.js
@@ -10,21 +10,35 @@ export default function Home() {
   return (
     <main className="occ-home">
       <Head>
-        <title>On chain collective project</title>
+        <title>On chain collective</title>
+        <meta name="title" content="On chain collective"/>
+        <meta name="description" content="Channeling our love for art and technology, and use completely on-chain tech to make beautiful NFTs for you to own or to share."/>
+
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://www.occ.xyz/"/>
+        <meta property="og:title" content="On chain collective"/>
+        <meta property="og:description" content="Channeling our love for art and technology, and use completely on-chain tech to make beautiful NFTs for you to own or to share."/>
+        <meta property="og:image" content=""/>
+
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://www.occ.xyz/"/>
+        <meta property="twitter:title" content="On chain collective"/>
+        <meta property="twitter:description" content="Channeling our love for art and technology, and use completely on-chain tech to make beautiful NFTs for you to own or to share."/>
+        <meta property="twitter:image" content=""/>
         <link rel="icon" href="/occ/favicon.ico" sizes="any" />
         <link rel="icon" href="/occ/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/occ/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.webmanifest" />
       </Head>
+
       <div className="flex align-center flex-col max-w-2xl mx-auto text-center mb-10 p-4">
-        <div className="mt-24">
+        <div className="mt-20">
           <header className="occ-logo oc-color text-5xl md:text-6xl">
             On chain collective
           </header>
-          <p className="text-xl mt-8">Our goal is to channel our love for art and technology, 
-          and use completely on-chain tech to make beautiful NFTs for you to own or to share.</p>
-          <p className="text-xl mt-4">Every NFT is 100% on chain, generated in the smart contract &amp; 
-            stored on the ethereum blockchain forever.</p>
+          <p className="text-xl mt-8">We channel our love for art and technology, and use completely on-chain tech to make beautiful NFTs for you to own or to share.</p>
+          <p className="text-xl mt-4">Every NFT is 100% on-chain, generated in the smart contract &amp; 
+          stored on the ethereum blockchain forever.</p>
         </div>
         <div className="mt-20 opacity-50">
           ~
