cdf4bc60fc934922f770693f8e7e36aaf0709d24
Nitin R Gupta <nitinramachandragupta@gmail.com>
2021-09-25 18:39:51 +0530

    margin fix

--- pages/flowers.js
+++ pages/flowers.js
@@ -9,65 +9,65 @@ export default function Home() {
   } );
   return (
     <main className="occ-home">
-      <Head>
-        <title>Flowers • OCC #1</title>
-        <meta name="title" content="Flowers • OCC #1"/>
-        <meta name="description" content="On-chain flower NFTs for you to own or to share."/>
+        <Head>
+            <title>Flowers • OCC #1</title>
+            <meta name="title" content="Flowers • OCC #1"/>
+            <meta name="description" content="On-chain flower NFTs for you to own or to share."/>
 
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://www.occ.xyz/flowers"/>
-        <meta property="og:title" content="Flowers • OCC #1"/>
-        <meta property="og:description" content="On-chain flower NFTs for you to own or to share."/>
-        <meta property="og:image" content="https://www.occ.xyz/flowers/social_image.png"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content="https://www.occ.xyz/flowers"/>
+            <meta property="og:title" content="Flowers • OCC #1"/>
+            <meta property="og:description" content="On-chain flower NFTs for you to own or to share."/>
+            <meta property="og:image" content="https://www.occ.xyz/flowers/social_image.png"/>
 
-        <meta property="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:url" content="https://www.occ.xyz/flowers"/>
-        <meta property="twitter:title" content="Flowers • OCC #1"/>
-        <meta property="twitter:description" content="On-chain flower NFTs for you to own or to share."/>
-        <meta property="twitter:image" content="https://www.occ.xyz/flowers/social_image.png"/>
-        <link rel="icon" href="/flowers/favicon.ico" sizes="any" />
-        <link rel="icon" href="/flowers/icon.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/flowers/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.webmanifest" />
-      </Head>
+            <meta property="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:url" content="https://www.occ.xyz/flowers"/>
+            <meta property="twitter:title" content="Flowers • OCC #1"/>
+            <meta property="twitter:description" content="On-chain flower NFTs for you to own or to share."/>
+            <meta property="twitter:image" content="https://www.occ.xyz/flowers/social_image.png"/>
+            <link rel="icon" href="/flowers/favicon.ico" sizes="any" />
+            <link rel="icon" href="/flowers/icon.png" type="image/png" />
+            <link rel="apple-touch-icon" href="/flowers/apple-touch-icon.png" />
+            <link rel="manifest" href="/manifest.webmanifest" />
+        </Head>
       
-    <div className="flex align-center flex-col max-w-2xl mx-auto text-center p-4">
-        <div className="mt-20">
-          <header className="text-5xl md:text-6xl font-snell ml-8">
-            flowers
-          </header>
-          <h3 className="mt-6">Fully on-chain generative NFTs</h3>
+        <div className="flex align-center flex-col max-w-2xl mx-auto text-center p-4">
+            <div className="mt-20">
+            <header className="text-5xl md:text-6xl font-snell ml-8">
+                flowers
+            </header>
+            <h3 className="mt-6">Fully on-chain generative NFTs</h3>
+            </div>
         </div>
-    </div>
-    <div className="mt-20 max-w-6xl mx-auto text-center flex flex-col md:flex-row justify-center items-center flower-tease px-4">
-        <div className="w-full rounded-2xl p-4">
-            <object data="/flowers/middleflower.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
+        <div className="mt-10 max-w-6xl mx-auto text-center flex flex-col md:flex-row justify-center items-center flower-tease px-4">
+            <div className="w-full rounded-2xl p-4">
+                <object data="/flowers/middleflower.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
+            </div>
+            <div className="w-full rounded-2xl p-4">
+                <object data="/flowers/leftflower.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
+            </div>
+            <div className="w-full rounded-2xl p-4">
+                <object data="/flowers/rightflower.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
+            </div>
         </div>
-        <div className="w-full rounded-2xl p-4">
-            <object data="/flowers/leftflower.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
+        <div className="flex align-center flex-col max-w-2xl mx-auto text-center mt-10 p-4">
+            <ul>
+                <li><h3 className="quotes text-xl">“There are always flowers for those who want to see them” ~ Henri Matisse</h3></li>
+                {/* <li><h3 className="quotes">“Every flower is a soul blossoming in nature” ~ Gerard De Nerwal</h3></li>
+                <li><h3 className="quotes">“In joy and in sadness, flowers are our constant friends” ~ Unknown</h3></li> */}
+            </ul>
         </div>
-        <div className="w-full rounded-2xl p-4">
-            <object data="/flowers/rightflower.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
+        <div className="flex align-center flex-col max-w-2xl mx-auto text-center mt-2 mb-10 p-4">
+            <div className="text-md ">
+            <a href="https://twitter.com/OnChainCo" target="_blank" className="hover:underline">
+                twitter
+            </a>{" "}
+            &bull;{" "}
+            <a href="https://discord.com/invite/BUCup66VKc" target="_blank" className="hover:underline">
+                discord
+            </a>{" "}
+            </div>
         </div>
-    </div>
-    <div className="flex align-center flex-col max-w-2xl mx-auto text-center mt-20 p-4">
-        <ul>
-            <li><h3 className="quotes text-xl">“There are always flowers for those who want to see them” ~ Henri Matisse</h3></li>
-            {/* <li><h3 className="quotes">“Every flower is a soul blossoming in nature” ~ Gerard De Nerwal</h3></li>
-            <li><h3 className="quotes">“In joy and in sadness, flowers are our constant friends” ~ Unknown</h3></li> */}
-        </ul>
-    </div>
-    <div className="flex align-center flex-col max-w-2xl mx-auto text-center mt-2 mb-10 p-4">
-        <div className="text-md ">
-          <a href="https://twitter.com/OnChainCo" target="_blank" className="hover:underline">
-            twitter
-          </a>{" "}
-          &bull;{" "}
-          <a href="https://discord.com/invite/BUCup66VKc" target="_blank" className="hover:underline">
-            discord
-          </a>{" "}
-        </div>
-      </div>
     </main>
   );
 }
