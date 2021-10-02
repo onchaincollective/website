021830d8276979bc792965369259d2c27bb574ba
Nitin R Gupta <nitinramachandragupta@gmail.com>
2021-10-02 17:44:17 +0530

    Added new flowers

--- pages/flowers.js
+++ pages/flowers.js
@@ -1424,8 +1424,11 @@ function Home() {
 
 
         <div className="mt-10 max-w-6xl mx-auto text-center flex flex-col md:flex-row justify-center items-center flower-tease px-4">
+            {/* <div className="w-full rounded-2xl p-4">
+                <object data="/flowers/1.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
+            </div> */}
             <div className="w-full rounded-2xl p-4">
-                <object data="/flowers/middleflower.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
+                <object data="/flowers/3.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
             </div>
             <div className="w-full rounded-2xl p-4">
                 <object data="/flowers/4.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
@@ -1442,7 +1445,21 @@ function Home() {
                 <object data="/flowers/7.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
             </div>
             <div className="w-full rounded-2xl p-4">
-                <object data="/flowers/rightflower.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
+                <object data="/flowers/5.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
+            </div>
+        </div>
+        <div className="mt-2 max-w-6xl mx-auto text-center flex flex-col md:flex-row justify-center items-center flower-tease px-4">
+            <div className="w-full rounded-2xl p-4">
+                <object data="/flowers/6.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
+            </div>
+            <div className="w-full rounded-2xl p-4">
+                <object data="/flowers/7.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
+            </div>
+            {/* <div className="w-full rounded-2xl p-4">
+                <object data="/flowers/2.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
+            </div> */}
+            <div className="w-full rounded-2xl p-4">
+                <object data="/flowers/8.svg" type="image/svg+xml" className="w-full rounded-2xl"></object>
             </div>
         </div>
         <div className="mt-2 max-w-6xl mx-auto text-center flex flex-col md:flex-row justify-center items-center flower-tease px-4">
